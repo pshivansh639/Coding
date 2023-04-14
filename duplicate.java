f9bf486596e09105b67ddc5af3f8efb7cf7b1564
Shivansh Pathak <91477357+pshivansh639@users.noreply.github.com>
2023-04-14 20:03:36 +0530

    Update duplicate.java

--- duplicate.java
+++ duplicate.java
@@ -1,19 +1,35 @@
-class Leet442 {
+class duplicate_or_not {
   
     public static void main(String args[])
     {
-        int numRay[] = { 0, 4, 3, 2, 7, 8, 2, 3, 1 };
-  
-        for (int i = 0; i < numRay.length; i++) {
-            numRay[numRay[i] % numRay.length]
-                = numRay[numRay[i] % numRay.length]
-                  + numRay.length;
-        }
-        System.out.println("The repeating elements are : ");
-        for (int i = 0; i < numRay.length; i++) {
-            if (numRay[i] >= numRay.length * 2) {
-                System.out.println(i + " ");
+        int numRay[] = { 0, 4, 3, 7, 8, 2, 1 };
+        int c=0;
+        int d=0;
+        for(int i=0;i<numRay.length;i++)
+        {
+            for(int j=0;j<numRay.length;j++)
+            {
+                if(numRay[i]==numRay[j])
+                {
+                    c++;
+                }
+                
             }
+            d++;
+            if(c>=2)
+                {
+                    System.out.println("it contains duplicate");
+                    
+                    break;
+                    
+                }
+                
+            c=0;
         }
+        if(d==numRay.length)
+        {
+            System.out.println("not contains duplicate");
+        }
+        
     }
 }
