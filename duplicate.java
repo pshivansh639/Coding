c855fa13f03c1d8582940f35a553654397921402
Shivansh Pathak <91477357+pshivansh639@users.noreply.github.com>
2023-04-16 11:33:16 +0530

    Update duplicate.java

--- duplicate.java
+++ duplicate.java
@@ -1,35 +1,10 @@
-class duplicate_or_not {
-  
-    public static void main(String args[])
-    {
-        int numRay[] = { 0, 4, 3, 7, 8, 2, 1 };
-        int c=0;
-        int d=0;
-        for(int i=0;i<numRay.length;i++)
-        {
-            for(int j=0;j<numRay.length;j++)
-            {
-                if(numRay[i]==numRay[j])
-                {
-                    c++;
-                }
-                
-            }
-            d++;
-            if(c>=2)
-                {
-                    System.out.println("it contains duplicate");
-                    
-                    break;
-                    
-                }
-                
-            c=0;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; ++i) {
+            //Checking the adjacent element for equality
+            if (nums[i] == nums[i + 1]) return true;
         }
-        if(d==numRay.length)
-        {
-            System.out.println("not contains duplicate");
-        }
-        
+        return false;
     }
 }
